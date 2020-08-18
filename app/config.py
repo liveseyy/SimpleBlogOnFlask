84dc52a00a353c8f6caa62cd28032db97ec6482c
liveseyy <dima74ermilov@gmail.com>
2020-08-18 19:00:28 +0500

    Изменения: 1. Добавлен navbar 2. Создана таблица в базе данных mySQL, при помощи SQLAlchhemy посты добавляются в таблицу со следующими заголовками: id, title, slug, body, created

--- app/config.py
+++ app/config.py
@@ -1,2 +1,4 @@
 class Configuration:
-	DEBUG = True
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:root@localhost/flask_blog'
