354cba468b13af4d20ece13c6e7cc00673f90718
liveseyy <dima74ermilov@gmail.com>
2020-08-19 15:52:47 +0500

    Изменения: 1. Добавлена таблция для миграции 2. Добавлена таблцица тегов 3. Добавлена таблица связывающая id постов с id тегов (ManyToMany) 4. На странице поста показываются соответсвующие теги

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-"""Формирование поста для базы данных"""
+"""Формирование поста и тегов для базы данных"""
 
 from app import db
 from datetime import datetime
@@ -8,6 +8,12 @@ import re
 def slugify(s):
     pattern = r'[^\w+]'
     return re.sub(pattern, '-', s)
+
+
+post_tags = db.Table('post_tags',  # Новая таблица в бд, связывающая теги и посты
+                      db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                      db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                     )
 
 
 class Post(db.Model):
@@ -21,9 +27,24 @@ class Post(db.Model):
         super(Post, self).__init__(*args, **kwargs)
         self.generate_slug()
 
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
+
     def generate_slug(self):
         if self.title:
             self.slug = slugify(self.title)
 
     def __repr__(self):
         return f'<Post id: {self.id}, title: {self.title}>'
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return f'<Tag id: {self.id}, name: {self.name}>'
--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,8 +1,8 @@
-"""Страница blog, содержащая посты, и определение Blueprint страницы posts"""
+"""Страница blog, содержащая посты и теги"""
 from flask import Blueprint
 from flask import render_template
 
-from models import Post
+from models import Post, Tag
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
@@ -15,6 +15,13 @@ def index():
 
 @posts.route('/<slug>')
 def post_detail(slug):
-    post = Post.query.filter(Post.slug==slug).first()
-    return render_template('posts/post_detail.html', post=post)
+    post = Post.query.filter(Post.slug == slug).first()
+    tags = post.tags
+    return render_template('posts/post_detail.html', post=post, tags=tags)
 
+
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
