5cfc4973732287a78de3fd906caf89a95475cbdc
liveseyy <dima74ermilov@gmail.com>
2020-08-21 12:27:53 +0500

    Добавлено: 1. Возможность редактирования поста через ./blog/<название поста>/edit 2. Админка в localhost/admin, для просмотра и ред. постов и тегов

--- app/app.py
+++ app/app.py
@@ -7,6 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
@@ -17,3 +20,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+### ADMIN ###
+from models import *
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:root@localhost/flask_blog'
+    SECRET_KEY = 'something very secret'
