63fe9b0d6fd16b806f04456f7580c0791a9c1862
liveseyy <dima74ermilov@gmail.com>
2020-08-22 11:34:39 +0500

    Изменения: 1. Теперь только авторизовавшиеся администраторы могут создавать и ред. посты и теги 2. Исправлен баг с слагом при создании поста через админ меню 3. Улучшен внешний вид авторизации в админ меню 4. В админ меню при создании постов и тегов удалены автоматически создающиеся критерии(слаг, дата создания)

--- app/app.py
+++ app/app.py
@@ -8,10 +8,14 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 from flask_admin import Admin
+from flask_admin import AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 
 from flask_security import SQLAlchemyUserDatastore
 from flask_security import Security
+from flask_security import current_user
+
+from flask import redirect, url_for, request
 
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine
@@ -26,16 +30,48 @@ Session = scoped_session(sessionmaker(bind=some_engine))
 session = Session()
 
 
-
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 # ADMIN
 from models import *
-admin = Admin(app)
-admin.add_view(ModelView(Post, db.session))
-admin.add_view(ModelView(Tag, db.session))
+
+
+class AdminMixin:
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('security.login', next=request.url))
+
+
+class BaseModelView(ModelView):
+    def on_model_change(self, form, model, is_created):
+        model.generate_slug()
+        return super(BaseModelView, self).on_model_change(form, model, is_created)
+
+
+class AdminView(AdminMixin, ModelView):
+    pass
+
+
+class HomeAdminView(AdminMixin, AdminIndexView):
+    pass
+
+
+class PostAdminView(AdminMixin, BaseModelView):
+    form_columns = ['title', 'body', 'tags']
+
+
+class TagAdminView(AdminMixin, BaseModelView):
+    form_columns = ['name', 'posts']
+
+admin = Admin(app, 'FlaskApp', url='/', index_view=HomeAdminView(name='Home'))
+admin.add_view(PostAdminView(Post, db.session))
+admin.add_view(TagAdminView(Tag, db.session))
+
+
 
 # Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
